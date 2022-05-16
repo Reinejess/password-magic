16468068d59886a97178a5389da8e45baed2b136
Reinejess <lore.jessmedina@gmail.com>
2022-05-16 18:19:10 -0500

    variables

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,10 @@
 // Assignment code here
-
+// set variables //
+var password = []
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var specialChar = "!@#$%^&*<>?{}-+"
+var num = "0123456789"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,6 +17,13 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword () {
+  var length = prompt("Please choose how many characters you would like your password to contain. You can choose between 8 and 128 characters.");
+}
+  if (length < 8 || length > 128) {
+    alert("Not within the password criteria. Try again.");
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
